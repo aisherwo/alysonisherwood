be186a603691d08e5b5177fd91323d1f37b0aa9f
aisherwo <43270225+aisherwo@users.noreply.github.com>
2020-06-11 22:28:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -428,19 +428,4 @@
 })(jQuery);
 
 
-$(function () {
-
-    // Initate masonry grid
-    var $grid = $('.gallery-wrapper').masonry({
-        temSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        percentPosition: true,
-    });
-
-    // Initate imagesLoaded
-    $grid.imagesLoaded().progress( function() {
-        $grid.masonry('layout');
-    });
-    
-});
 
