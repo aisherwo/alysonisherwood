d45069a18d65f2111bdc7e253630238a9576c79f
aisherwo <43270225+aisherwo@users.noreply.github.com>
2020-06-11 22:27:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -426,3 +426,21 @@
  
 
 })(jQuery);
+
+
+$(function () {
+
+    // Initate masonry grid
+    var $grid = $('.gallery-wrapper').masonry({
+        temSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+    });
+
+    // Initate imagesLoaded
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry('layout');
+    });
+    
+});
+
